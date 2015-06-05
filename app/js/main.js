809329468d88b80f44392f8372dc8ae9b6bcd025
jim leach <yogajim@gmail.com>
2015-06-05 09:26:46 -0400

    intial commit

---
+++ app/js/main.js
@@ -0,0 +1,42 @@
+
+// (function(){
+
+ var Todo = function(options){
+ var args = options || {};
+
+ this.task = options.task;
+ this.status = 'Open';
+
+};
+var storageBin = [];
+
+// var a = new Todo({task: taskText});
+
+//Set up submit
+//on submit create a new instance
+
+$('#addTask').on('submit', function(event){
+ event.preventDefault();
+ var taskText = $('#taskText').val();
+ var taskInstance = new Todo({task: taskText});
+ storageBin.push(taskInstance);
+ $('#tasks').append('<li>' + taskText + '</li>');
+ this.reset();
+});
+
+// toggle item
+
+$('#tasks').on('click', 'li', function(){
+ event.preventDefault();
+
+ //grab the list item I clicked on
+ //mark that item as completed
+ $(this).addClass('complete');
+ var tTask= $(this).text();
+ var taskToEdit = _.find(storageBin, { task: tTask });
+ taskToEdit.status = 'Closed';
+ console.log(taskToEdit);
+});
+
+// }());
+
